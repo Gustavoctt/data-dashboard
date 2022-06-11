aac7dd6524b708910589faf1f1f109f459a50320
Gustavoctt <gustavocarrertartare@gmail.com>
2022-06-10 22:37:32 -0300

    header component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from './components/Header';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <>
       <GlobalStyle/>
-      <h1>Iniciando</h1>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  background: #CEC6CE;
+
+  margin: 0 auto;
+  padding: 0rem 0rem 5.6875rem;
+`
