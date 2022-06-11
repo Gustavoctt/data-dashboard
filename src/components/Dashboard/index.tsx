b9f2b71f1473e6bc54900fbde72d6267508a2cb6
Gustavoctt <gustavocarrertartare@gmail.com>
2022-06-10 22:48:26 -0300

    create dashboard component

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+export function Dashboard(){
+  return(
+    <Container>
+      <p>Text</p>
+    </Container>
+  )
+}
---
+++ src/components/Dashboard/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  margin: 0 auto;
+  padding: 2.06rem 1.75rem;
+`
